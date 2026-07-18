c8511e6479da010d3736483d340c0c73be5352bb
codewithmohitsings <mohitrath42@gmail.com>
2026-07-18 23:47:30 +0530

    Added a featurew scroll to top

---
+++ src/components/ScrollToTop.jsx
@@ -0,0 +1,3 @@
+export default function ScrollToTop() {
+  return <a className="outer scroll-to-top scroll" href="#wrapper"><div className="scroll-to-top-inner inner radius-2"><span className="fas fa-arrow-up top-icon"></span></div></a>;
+}
